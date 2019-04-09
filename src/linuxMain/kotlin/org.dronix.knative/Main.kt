29550e6105544bc10bcec61489315ffe77d02860
harlem88 <harlem88.ant@gmail.com>
2019-04-10 00:12:34 +0200

    first commit

--- src/linuxMain/kotlin/org.dronix.knative/Main.kt
+++ src/linuxMain/kotlin/org.dronix.knative/Main.kt
@@ -1,12 +1,12 @@
 package org.dronix.knative
 
-import org.dronix.knative.data.action.GitHubCommandAction
+import org.dronix.knative.data.action.GitCommandAction
 import org.dronix.knative.data.retriever.MockCommandRetriever
 import org.dronix.knative.domain.CommandUseCase
 
 fun main() {
 
     println( " ====== START =====")
-    val commandUseCase = CommandUseCase(MockCommandRetriever(), GitHubCommandAction("origin", "develop"))
+    val commandUseCase = CommandUseCase(MockCommandRetriever(), GitCommandAction("origin", "develop"))
     commandUseCase.startReceiveCommand()
 }
--- src/linuxMain/kotlin/org.dronix.knative/data/action/GitCommandAction.kt
+++ src/linuxMain/kotlin/org.dronix.knative/data/action/GitCommandAction.kt
@@ -2,7 +2,7 @@ package org.dronix.knative.data.action
 
 import org.dronix.knative.domain.CommandAction
 
-class GitHubCommandAction(private val origin: String, private val branch: String): CommandAction{
+class GitCommandAction(private val origin: String, private val branch: String): CommandAction{
     override fun onPull() {
         platform.posix.system("git pull $origin $branch")
     }
